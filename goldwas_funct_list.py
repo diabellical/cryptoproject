93af740576da0fd6e4f6a994aeb3fecf6c757add
Jean Baptiste <diabellical@gmail.com>
2015-04-26 20:07:48 -0500

    Create goldwas_funct_list.py

---
+++ goldwas_funct_list.py
@@ -0,0 +1,14 @@
+## https://gist.github.com/brunoro/5893701 (see that for complete non homo implem) 
+
+#functnames and skels no defs
+
+#!/usr/bin/env python
+# encoding: utf8
+
+#imports (etc) 
+from unicodedata import normalize 
+from string import ascii_letters
+from random import randint
+#
+
+#primality test of your choise
